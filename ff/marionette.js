42a46367e08ddecc689f396661e167d8324b20bb
grave4save <44759886+grave4save@users.noreply.github.com>
2018-12-13 03:57:47 +0000

    Add files via upload

--- ff/marionette.js
+++ ff/marionette.js
@@ -1,5 +1,4 @@
 
 pref("marionette.enabled",false);
-pref("marionette.debugging.clicktostart",false);pref("marionette.log.level","Info");
-pref("marionette.log.truncate",true);pref("marionette.port",2828);pref("marionette.prefs.recommended",true);
+pref("marionette.debugging.clicktostart",false);pref("marionette.log.level","Info");pref("marionette.log.truncate",true);pref("marionette.port",2828);pref("marionette.prefs.recommended",true);
 pref("marionette.contentListener",false);
