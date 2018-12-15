f8d425423826cd56db93287ec16606875905f061
grave4save <44759886+grave4save@users.noreply.github.com>
2018-12-15 10:06:16 +0000

    mobile 64 (#32)

--- mobile/marionette.js
+++ mobile/marionette.js
@@ -1,4 +1,3 @@
 pref("marionette.enabled",false);
-pref("marionette.debugging.clicktostart",false);pref("marionette.log.level","Info");
-pref("marionette.log.truncate",true);pref("marionette.port",2828);pref("marionette.prefs.recommended",true);
+pref("marionette.debugging.clicktostart",false);pref("marionette.log.level","Info");pref("marionette.log.truncate",true);pref("marionette.port",2828);pref("marionette.prefs.recommended",true);
 pref("marionette.contentListener",false);
