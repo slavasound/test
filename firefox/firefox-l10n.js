0a9f945305c9850501683547e3214f00359029b0
grave4save <44759886+grave4save@users.noreply.github.com>
2018-12-15 04:55:34 +0000

    Delete firefox-l10n.js

--- firefox/firefox-l10n.js
+++
@@ -1,4 +0,0 @@
-//@line 4 "/builds/worker/workspace/build/src/obj-firefox/browser/locales/merge-dir/ru/browser/firefox-l10n.js"
-//@line 6 "/builds/worker/workspace/build/src/obj-firefox/browser/locales/merge-dir/ru/browser/firefox-l10n.js"
-//@line 10 "/builds/worker/workspace/build/src/obj-firefox/browser/locales/merge-dir/ru/browser/firefox-l10n.js"
-pref("browser.search.geoSpecificDefaults", true);
