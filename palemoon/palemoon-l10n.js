a49a679cec9dd8586672d0e1f5b52498a28871e9
grave4save <44759886+grave4save@users.noreply.github.com>
2019-01-16 18:13:46 +0000

    Delete palemoon-l10n.js

--- palemoon/palemoon-l10n.js
+++
@@ -1,5 +0,0 @@
-//@line 4 "d:\mozdev\UXP\application\palemoon\locales\en-US\palemoon-l10n.js"
-
-//@line 6 "d:\mozdev\UXP\application\palemoon\locales\en-US\palemoon-l10n.js"
-
-pref("general.useragent.locale", "en-US");
