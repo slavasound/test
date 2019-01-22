d8c4c4fdddabcf34f2389e2f2d73847e268ad00e
grave4save <44759886+grave4save@users.noreply.github.com>
2019-01-22 07:23:47 +0300

    th 60.4.0

---
+++ thunderbird/all-l10n.js
@@ -0,0 +1,6 @@
+//@line 4 "/builds/worker/workspace/build/l10n/ru/mail/all-l10n.js"
+
+//@line 6 "/builds/worker/workspace/build/l10n/ru/mail/all-l10n.js"
+
+pref("general.useragent.locale", "ru");
+pref("spellchecker.dictionary", "RU");
