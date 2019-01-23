712185f99e57b8ba19c7f66e23d187db442eb9fc
grave4save <44759886+grave4save@users.noreply.github.com>
2019-01-24 00:28:47 +0300

    pl 28.3.0

---
+++ palemoon/palemoon-l10n.js
@@ -0,0 +1,5 @@
+//@line 4 "d:\mozdev\UXP\application\palemoon\locales\en-US\palemoon-l10n.js"
+
+//@line 6 "d:\mozdev\UXP\application\palemoon\locales\en-US\palemoon-l10n.js"
+
+pref("general.useragent.locale", "en-US");
