9177abc5696886afe6bd60dec0fbbb53ebd6f476
slavasound <44759886+slavasound@users.noreply.github.com>
2019-02-01 03:14:58 +0000

    wf 56.2.6

---
+++ waterfox/firefox-l10n.js
@@ -0,0 +1,7 @@
+//@line 4 "/home/linuxbox/Waterfox/browser/locales/en-US/firefox-l10n.js"
+
+//@line 8 "/home/linuxbox/Waterfox/browser/locales/en-US/firefox-l10n.js"
+
+//@line 11 "/home/linuxbox/Waterfox/browser/locales/en-US/firefox-l10n.js"
+pref("browser.search.geoSpecificDefaults", false);
+
