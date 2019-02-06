32bed5b1ca33e276e4e258bca30d847e5948fc79
slavasound <slavasound@yandex.ru>
2019-02-06 12:22:03 +0000

    rm

--- waterfox/firefox-l10n.js
+++
@@ -1,7 +0,0 @@
-//@line 4 "c:\Work\Waterfox\browser\locales\en-US\firefox-l10n.js"
-
-//@line 8 "c:\Work\Waterfox\browser\locales\en-US\firefox-l10n.js"
-
-//@line 11 "c:\Work\Waterfox\browser\locales\en-US\firefox-l10n.js"
-pref("browser.search.geoSpecificDefaults", false);
-
