274dedf638da0478153fbae590e243541b0b1031
slavasound <44759886+slavasound@users.noreply.github.com>
2019-02-13 14:32:27 +1100

    firefox 65

--- firefoxmobi/marionette.js
+++ firefoxmobi/marionette.js
@@ -1,7 +1,7 @@
-pref("marionette.enabled", false);
+pref("marionette.contentListener", false);
 pref("marionette.debugging.clicktostart", false);
+pref("marionette.enabled", false);
 pref("marionette.log.level", "Info");
 pref("marionette.log.truncate", true);
 pref("marionette.port", 2828);
 pref("marionette.prefs.recommended", true);
-pref("marionette.contentListener", false);
