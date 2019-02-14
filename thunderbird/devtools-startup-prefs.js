6d2e9cf04729efc4c2560e39327f2f5b7253e3a0
slavasound <44759886+slavasound@users.noreply.github.com>
2019-02-15 09:22:42 +1100

    th 65.0.1

---
+++ thunderbird/devtools-startup-prefs.js
@@ -0,0 +1,7 @@
+pref("devtools.jsonview.enabled", true);
+pref("devtools.onboarding.experiment", "off");
+pref("devtools.onboarding.experiment.flipped", false);
+pref("devtools.onboarding.telemetry.logged", false);
+pref("devtools.policy.disabled", false);
+pref("devtools.theme", "light", sticky);
+pref("devtools.toolbar.visible", false);
---
+++ thunderbird/thunderbird-branding.js
@@ -0,0 +1,7 @@
+pref("app.update.download.backgroundInterval", 600);
+pref("app.update.interval", 86400);
+pref("app.update.promptWaitTime", 86400);
+pref("app.vendorURL", "https://www.thunderbird.net/%LOCALE%/");
+pref("browser.search.param.ms-pc", "MOZT");
+pref("mailnews.start_page.override_url","https://live.thunderbird.net/%APP%/whatsnew?locale=%LOCALE%&version=%VERSION%&channel=%CHANNEL%&os=%OS%&buildid=%APPBUILDID%");
+pref("mailnews.start_page.url","https://live.thunderbird.net/%APP%/start?locale=%LOCALE%&version=%VERSION%&channel=%CHANNEL%&os=%OS%&buildid=%APPBUILDID%");
