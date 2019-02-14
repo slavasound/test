b7c5c11a789f81400ff2e441ec434c8d2b006535
slavasound <44759886+slavasound@users.noreply.github.com>
2019-02-15 09:38:43 +1100

    1

---
+++ th/all-l10n.js
@@ -0,0 +1,5 @@
+//@line 4 "/builds/worker/workspace/build/src/comm/mail/locales/en-US/all-l10n.js"
+
+//@line 6 "/builds/worker/workspace/build/src/comm/mail/locales/en-US/all-l10n.js"
+
+pref("spellchecker.dictionary", "en-US");
