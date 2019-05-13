11478e06f5a0298964cfc0e460683432d0ecac60
slavasound <slavasaund@yandex.ru>
2019-05-14 01:32:17 +0300

    rm
    Signed-off-by: slavasound <slavasaund@yandex.ru>

--- fox/firefox-l10n.js
+++
@@ -1,2 +0,0 @@
-//@line 4 "/builds/worker/workspace/build/src/browser/locales/en-US/firefox-l10n.js"
-
--- thundr/mdn.js
+++
@@ -1,9 +0,0 @@
-pref("mail.identity.default.request_receipt_header_type", 0);
-pref("mail.identity.default.request_return_receipt_on", !1);
-pref("mail.identity.default.use_custom_prefs", !1);
-pref("mail.server.default.incorporate_return_receipt", 0);
-pref("mail.server.default.mdn_not_in_to_cc", 2);
-pref("mail.server.default.mdn_other", 2);
-pref("mail.server.default.mdn_outside_domain", 2);
-pref("mail.server.default.mdn_report_enabled", !0)
-pref("mail.server.default.mdn_report_enabled", !0);
